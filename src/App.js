ac0954ef9f429e07f39dc4227da75ad68084e8f3
h0uss3m <hossem.benmustapha@gmail.com>
2021-03-22 11:53:17 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <h1>Welcome Home</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
